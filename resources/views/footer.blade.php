2f318ff055d980719428d2396ff8c22c17bdf6b9
kaneki13383 <danchik.kun@mail.ru>
2023-05-07 02:49:17 +0400

    Сверстал главную header и footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,25 @@
+<footer class="">
+    <div class="info container">
+        <div>
+            <h5>Покупателям</h5>
+            <p>Личный кабинет</p>
+        </div>
+        <div>
+            <h5>Информация</h5>
+            <p>О нас</p>
+            <p>Главная</p>
+        </div>
+        <div>
+            <h5>Связь</h5>
+            <p>+7 (967) 331 27-86</p>
+        </div>
+        <div>
+            <h5>Подписка на новости</h5>
+            <p>Получите доступ к эксклюзивным скидкам</p>
+            <input type="email" name="" id="" placeholder="E-mail">
+        </div>
+    </div>
+    <div class="copyright">
+        <p>Furnistore © 2023</p>
+    </div>
+</footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/header.css">
+    <link rel="stylesheet" href="/css/footer.css">
     <link rel="stylesheet" href="/css/welcome.css">
     <title>Главная</title>
 </head>
@@ -29,7 +30,41 @@
 
     <div class="container section_second">
         <h2>Необычные и уникальные решения</h2>
+        <div>
+            <div>
+                <p>Green Couch</p>
+                <img width="60%" src="/images/divan-1.png" alt="">
+            </div>
+            <div>
+                <p>Sofa With Coushion</p>
+                <img width="50%" src="/images/divan-3.png" alt="">
+            </div>
+        </div>
+        <div>
+            <div>
+                <p>Textile Yellow</p>
+                <img width="40%" src="/images/divan-5.png" alt="">
+            </div>
+            <div>
+                <p>Side Ottoman</p>
+                <img width="30%" src="/images/divan-4.png" alt="">
+            </div>
+        </div>
     </div>
+
+    <div class="container section_third">
+        <div>
+            <div>
+                <p>Специальное предложение</p>
+                <h4>Wood Cloth Sofa</h4>
+                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia</p>
+                <button>Купить за 29 990 руб.</button>
+            </div>
+            <img width="70%" src="/images/feature_product_img.png" alt="">
+        </div>
+    </div>
+
+    @include('footer')
 
     <script src="../js/burger.js"></script>
 </body>
